848a69de203bef2e5624bb09bb98b1caf4706233
mordnerd <dadamordecai442@gmail.com>
2026-06-27 21:58:38 -0400

    Implement mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("navList");
+
+hamburger.addEventListener("click", () => {
+    navList.classList.toggle("show");
+});
+
+const links = document.querySelectorAll(".nav-list a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navList.classList.remove("show");
+    });
+});
